a812dc91ade53182339251bb8a2a361411ae3fcd
Michael Lacroix <kennycodes@hotmail.com>
2025-07-21 00:29:20 -0400

    Create MoviesList and MovieCard reusable component

---
+++ src/app/components/MovieCard.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function MovieCard() {
+    return (
+        <div>
+            <h1>Movie Card</h1>
+        </div>
+    )
+}
---
+++ src/app/components/MoviesList.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function MoviesList() {
+    return (
+        <div>
+            <h1>Movies List</h1>
+        </div>
+    )
+}
