ff49ebc025f3aebb263e48498b11f978ecfbb347
Michael Lacroix <kennycodes@hotmail.com>
2025-07-24 11:34:42 -0400

    Remove Suspense fallback from MovieDetailsPage rendering for simplified loading experience.

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,13 +1,10 @@
 import { getMovieById } from "@/api/movie";
 import MovieDetailsPage from "@/app/components/pages/MovieDetails";
-import { Suspense } from "react";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-    const movieId = Number.parseInt((await params).id as string) || 1;
-    const movie = await getMovieById(movieId);
-    return(
-      <Suspense fallback={<div>Loading...</div>}>
-        <MovieDetailsPage movie={movie} />
-      </Suspense>
-    )
+  const movieId = Number.parseInt((await params).id as string) || 1;
+  const movie = await getMovieById(movieId);
+  return (
+    <MovieDetailsPage movie={movie} />
+  )
 }
